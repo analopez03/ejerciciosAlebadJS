f31e6b5c63134b6698f6e297e41134990f3a537b
analopez03 <analopezdeahumada97@gmail.com>
2026-03-05 09:42:51 +0100

    Ejercicio 1 de javaScript

---
+++ Ejercicio1.js
@@ -0,0 +1,24 @@
+/*Ejercicio 1, Nomenclatura y tipo de variables
+Escribe una varible llamada user name utilizando las siguientes nomenclaturas:
+Camel case:
+Pascal case:
+Snake case:
+Kebab case:
+Define los siguientes tipos de variables, di cuál utilizarías y por qué.
+Var:
+Let:
+Const:*/
+
+// Camel case:
+let userName;
+
+// Pascal case:
+let UserName;
+
+// Snake case:
+let user_name;
+
+// Kebab case:
+let user-name; // No es válido en JavaScript
+
+// Var: Se utiliza para declarar variables que pueden ser reasignadas y tienen un alcance global o de función. Sin embargo, su uso no es recomendado debido a problemas de hoisting y alcance.
